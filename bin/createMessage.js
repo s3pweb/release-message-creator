d31f03cc2d17273e2b1dd1ffe7522824d4058a9b
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2022-05-13 15:38:48 +0200

    feat(discord): send extracted content to the discord API

--- bin/createMessage.js
+++ bin/createMessage.js
@@ -3,19 +3,20 @@
 
 const fs = require('fs')
 const util = require('util')
+const https = require('https')
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 
 const fileContent = fs.readFileSync(args[0], 'utf8').toString()
 
 const regex = new RegExp(/## \[.+?## \[/, 's')
 
-const res = fileContent.match(regex)
+const reducedContent = fileContent.match(regex)
 
 // Extract all the versions from the changelog
-const versions = [...fileContent.matchAll(new RegExp(/## \[(.*)\]/, 'gm'))]
+const versions = [...fileContent.matchAll(new RegExp(/## \[(.*)]/, 'gm'))]
 
-const changes = res[0]
+const changes = reducedContent[0]
   // Remove the last line
   .replace(/\n.*$/, '').trim()
   // Remove the commits links
@@ -28,4 +29,33 @@ const changes = res[0]
 // Format title by adding the current version and the old one
 const title = util.format(args[1], versions[0][1], versions[1][1])
 
-console.log(title + '\n\n' + changes)
+const data = JSON.stringify({
+  content: title + '\n\n' + changes
+})
+
+const options = {
+  hostname: 'discord.com',
+  port: 443,
+  path: args[2],
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length + 1,
+  },
+}
+
+// Call the discord API with the data
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
